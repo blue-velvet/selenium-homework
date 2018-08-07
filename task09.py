d8c50ecd34e51707dd5574495a1c583fc1c603b0
blue-velvet <31938637+blue-velvet@users.noreply.github.com>
2018-08-07 09:35:51 +0300

    minor changes

--- task09.py
+++ task09.py
@@ -7,7 +7,6 @@ import time
 @pytest.fixture
 def driver(request):
     wd = webdriver.Firefox(executable_path="C:\\Users\\KC\\PycharmProjects\\drivers\\geckodriver.exe")
-    print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
@@ -34,14 +33,15 @@ def test_check_sorting(driver):
             country.find_element_by_xpath("td[5]/a").send_keys(Keys.CONTROL + Keys.RETURN)
             time.sleep(1)
             driver.switch_to_window(driver.window_handles[1])
+
             list_of_zones = driver.find_elements_by_css_selector("table#table-zones tr:not(.header)")
 
             #for zone in list_of_zones:
-                #list2.append(zone.find_element_by_xpath("td[3]").text[0])
+                #list2.append(zone.find_element_by_xpath("td[3]").text)
             #print(list2)
 
-            if str(list2) == str(sorted(list2)):
-                 print("ZONES YES")
+            #if str(list2) == str(sorted(list2)):
+                #print("ZONES YES")
             driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
 
     if str(list1) == str(sorted(list1)):
